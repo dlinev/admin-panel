5e14f3efee152b52da7612caa7467db81c57062f
Dmitry Linev <97431699+dlinev@users.noreply.github.com>
2022-02-24 17:01:23 +0300

    Create react-app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,52 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <div class="frame frame_checkbox-radio">
+        <div class="checkbox-block">
+          <div class="checkbox">
+            <input class="custom-checkbox" type="checkbox" id="checkbox-1" />
+            <label for="checkbox-1"></label>
+          </div>
+          <div class="checkbox">
+            <input
+              class="custom-checkbox"
+              type="checkbox"
+              id="checkbox-2"
+              checked
+            />
+            <label for="checkbox-2"></label>
+          </div>
+        </div>
+      </div>
+
+      <div class="frame frame_checkbox-radio">
+        <div class="radio-block">
+          <div class="radio">
+            <input
+              class="custom-radio"
+              id="radio-1"
+              type="radio"
+              name="radio-group"
+              value="radio1"
+            />
+            <label for="radio-1"></label>
+          </div>
+          <div class="radio">
+            <input
+              class="custom-radio"
+              id="radio-2"
+              type="radio"
+              name="radio-group"
+              checked
+              value="radio2"
+            />
+            <label for="radio-2"></label>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
