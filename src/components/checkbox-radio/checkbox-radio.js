bc376a73f61960a28e15193e0ec8ea60a2156afb
Dmitry Linev <97431699+dlinev@users.noreply.github.com>
2022-03-16 16:19:26 +0300

    Update components

--- src/components/checkbox-radio/checkbox-radio.js
+++ src/components/checkbox-radio/checkbox-radio.js
@@ -4,7 +4,51 @@ import styles from './checkbox-radio.module.css';
 
 class CheckboxRadioBlock extends Component {    
     render() {
-        return 
+        return  <div>
+        <div className={`${styles.frame} ${styles.frame_checkboxRadio}`}>
+
+          <div className={styles.checkboxBlock}>
+            <div className={styles.checkbox}>
+              <input className={styles.customCheckbox} type="checkbox" id="checkbox1" />
+              <label for="checkbox1"></label>
+            </div>
+            <div className={styles.checkbox}>
+              <input
+                className={styles.customCheckbox}
+                type="checkbox"
+                id="checkbox2"
+              />
+              <label for="checkbox2"></label>
+            </div>
+          </div>
+      </div>
+  
+      <div className={`${styles.frame} ${styles.frame_checkboxRadio}`}>
+        <div className={styles.radioBlock}>
+          <div className={styles.radio}>
+            <input
+              className={styles.customRadio}
+              id="radio1"
+              type="radio"
+              name="radiogroup"
+              value="radio1"
+            />
+            <label for="radio1"></label>
+          </div>
+          <div className={styles.radio}>
+            <input
+              className={styles.customRadio}
+              id="radio2"
+              type="radio"
+              name="radiogroup"
+              checked
+              value="radio2"
+            />
+            <label for="radio2"></label>
+          </div>
+        </div>
+      </div>
+      </div>  
   }
 }
 export default CheckboxRadioBlock;
