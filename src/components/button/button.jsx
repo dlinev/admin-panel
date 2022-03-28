da5bb1afef36bb42533f85dd892194eb5cbffd45
Dmitry Linev <97431699+dlinev@users.noreply.github.com>
2022-03-28 14:17:17 +0300

    Update comp-w-props

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,59 +1,54 @@
-import cx from 'classnames';
-import { Image, Text } from '../components'
+import cx from "classnames";
+import { Image, Text } from "../components";
 
-import styles from './Button.module.css'; 
+import styles from "./Button.module.css";
 
-export const Button = ( { 
-  text, 
+export const Button = ({
+  text,
   icon,
   theme,
   size,
   className,
-  onClick=()=>{},
+  onClick = () => {},
   ...props
-} ) => {
-
-  if(!icon) {
-    switch (theme){
-      case 'disabled': {
-          icon = 'LockedIcon';
-          break;
+}) => {
+  if (!icon) {
+    switch (theme) {
+      case "disabled": {
+        icon = "LockedIcon";
+        break;
       }
       default: {
-          icon = 'XMediumIcon';
-          break;
+        icon = "XMediumIcon";
+        break;
       }
     }
   }
-  
 
-    const classNames = cx(styles._, className, {
-      [styles.theme_default]:  (theme==='default'),
-      [styles.theme_flat]:     (theme==='flat'),
-      [styles.theme_disabled]: (theme==='disabled'),
-      [styles.size_big]:       (size==='big'),
-      [styles.size_small]:     (size==='small'),
-      [styles.size_icon_only]: (!text),
-    });
+  const classNames = cx(styles._, className, {
+    [styles.theme_default]: theme === "default",
+    [styles.theme_flat]: theme === "flat",
+    [styles.theme_disabled]: theme === "disabled",
+    [styles.size_big]: size === "big",
+    [styles.size_small]: size === "small",
+    [styles.size_icon_only]: !text,
+  });
 
-    const imageClass = cx({
-      [styles.image_default]:  (theme==='default'),
-      [styles.image_error]:    (theme==='error'),
-      [styles.image_disabled]: (theme==='disabled'),
-    });   
+  const imageClass = cx({
+    [styles.image_default]: theme === "default",
+    [styles.image_error]: theme === "error",
+    [styles.image_disabled]: theme === "disabled",
+  });
 
-    const textClass = cx({
-      [styles.text_big]:   (size==='big' && icon!=='none'),
-      [styles.text_small]: (size==='small' && icon!=='none'),
-    });   
+  const textClass = cx({
+    [styles.text_big]: size === "big" && icon !== "none",
+    [styles.text_small]: size === "small" && icon !== "none",
+  });
 
-    return (
-      <button className={classNames} onClick={onClick} {...props}>
-        {icon && <Image className={imageClass} icon={icon} /> }
-        {text && <Text  className={textClass}  text={text} /> }
-      </button>
-
-    )
-
-
-  }
+  return (
+    <button className={classNames} onClick={onClick} {...props}>
+      {icon && <Image className={imageClass} icon={icon} />}
+      {text && <Text className={textClass} text={text} />}
+    </button>
+  );
+};
